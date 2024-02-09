e67a596155fa8d8761b9e17acb14c8519f779760
Caio Notebook <dev.meirelles22@gmail.com>
2024-02-08 21:16:24 -0300

    primeiras classes com taiwind

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,3 +1,22 @@
+import logo from './assets/logo-nlw-expert.svg'
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="logo" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <div className="rounded-md bg-slate-700"></div>
+        <div className="rounded-md bg-slate-800"></div>
+        <div className="rounded-md bg-slate-800"></div>
+      </div>
+    </div>
+  )
 }
